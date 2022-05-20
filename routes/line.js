8673c42a5520ffa90bd87d5b5d58f84767dcba70
siaochanwu <jill007tw@gmail.com>
2022-05-20 10:20:11 +0800

    feat: get coinapi and setting node-schedule

--- routes/line.js
+++ routes/line.js
@@ -1,43 +1,56 @@
-require('dotenv').config()
+require('dotenv').config();
+const schedule = require('node-schedule');
+const axios = require('axios').default;
 const express = require('express');
 const router = express.Router();
 const line = require('@line/bot-sdk');
 
 /* GET users listing. */
-router.post('/line', function(req, res, next) {
+router.post('/line', async (req, res, next) => {
 
     let events = req.body.events[0]
-    console.log(events)
-    const replyToken = events.replyToken
+    // console.log(events)
+    // const replyToken = events.replyToken;
+    pushMSG();
+    
+});
 
+function pushMSG() {
     const client = new line.Client({
         channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
     });
 
-    const message = {
-        type: 'text',
-        text: 'Hello World!'
-    };
-
-    client.replyMessage(replyToken, message)
-    .then(() => {
-        console.log('success')
-    })
-    .catch((err) => {
-        // error handling
-    });
-});
-
-function getCryptoPrice() {
-    fetch(`process.env.COIN_API?filter_asset_id=BTC;ETH`, {
-        method: 'GET',
+    axios({
+        url: `${process.env.COIN_API}?filter_asset_id=BTC,ETH`,
+        method: 'get',
         headers: {
             'X-CoinAPI-Key': process.env.COINAPI_Key
         },
     })
-    .then((res) => res.json())
-    .then((res) => console.log(res))
+    .then((res) => {
+        console.log(res)
+        const BTC = res.data[0].price_usd;
+        const ETH = res.data[1].price_usd;
+
+        const message = {
+            type: 'text',
+            text: `BTC: ${BTC} \nETH: ${ETH}`
+        };
+    
+        client.pushMessage(process.env.USER_ID, message)
+        .then(() => {
+            console.log('success')
+        })
+        .catch((err) => {
+            // error handling
+        });
+    })
     .catch((err) => console.error(err))
 }
 
+const job = schedule.scheduleJob('0 30 * * * *', function(){
+    console.log('The answer to life, the universe, and everything!');
+    pushMSG();
+  });
 module.exports = router;
+
